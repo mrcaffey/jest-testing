50772c2e96c51b5f836e390eed55d1e27d64f6f4
Micah Caffey <mrcaffey@gmail.com>
2018-10-04 13:44:08 -0600

    TodoList.test.js updated

--- src/__tests__/reducers/items.test.js
+++ src/__tests__/reducers/items.test.js
@@ -1,17 +1,17 @@
 import reducer from '../../reducers/items'
 
-const ADD_ITEM
-const REMOVE_ITEM
+const ADD_ITEM = 'ADD_ITEM'
+const REMOVE_ITEM = 'REMOVE_ITEM'
 
 describe('items reducer', () => {
   it('should return initial state', () => {
     expect(reducer(undefined, {})).toEqual([])
-})
+  })
 
-it('should handle ADD_ITEM', () => {
-  expect(
-    reducer([], { type: ADD_ITEM, name: 'Buy Milk' })
-  ).toEqual(['Buy Milk])
+  it('should handle ADD_ITEM', () => {
+    expect(
+      reducer([], { type: ADD_ITEM, name: 'Buy Milk' })
+    ).toEqual(['Buy Milk'])
   })
 
   it('should handle REMOVE_ITEM', () => {
